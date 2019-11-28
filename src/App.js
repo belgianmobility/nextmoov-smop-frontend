e8d10fca4ca16814eecb96196fe111954cd762fe
Mathieu <mathieu.f@nextmoov.be>
2019-11-28 14:42:12 +0100

    feat(App): add TextInput to App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
+// @flow
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import TextInput from './components/TextInput';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TextInput
+        onChange={(e) => console.log(e.target.value)}
+        id="from"
+        labelContent="from"
+        value="yolo"
+      />
     </div>
   );
 }
