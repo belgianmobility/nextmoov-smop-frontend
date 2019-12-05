d5bce2022ff1e43b75a05d99cc7840c8242f4f6f
Mathieu <mathieu.f@nextmoov.be>
2019-12-05 11:48:47 +0100

    refactor(TextInput): add state and prefilled value support

--- src/components/TextInput/index.js
+++ src/components/TextInput/index.js
@@ -1,32 +1,50 @@
 // @flow
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import s from './style.module.scss';
 
 type Props = {
   onChange?: Function,
   labelContent: string,
-  value: string,
   id: string,
   placeholder?: string,
+  inputValue?: string,
 }
 
 function TextInput(props: Props) {
   const {
     onChange,
     labelContent,
-    value,
-    id,
     placeholder,
+    inputValue,
+    id,
   } = props;
+
+  const [value, setValue] = useState('');
+
+
+  useEffect(() => {
+    if (inputValue !== '') {
+      setValue(inputValue);
+    }
+  }, [inputValue]);
+
+
+  function handleChange(e) {
+    setValue(e.target.value);
+    onChange(e);
+  }
+
   return (
     <label htmlFor={id} className={s.textinput__label}>
       {labelContent}
       <input
         id={id}
         value={value}
-        onChange={onChange}
+        onChange={handleChange}
         className={s.textinput__input}
         placeholder={placeholder}
+        type="text"
+        autoComplete="off"
       />
     </label>
   );
@@ -35,6 +53,7 @@ function TextInput(props: Props) {
 TextInput.defaultProps = {
   onChange: () => {},
   placeholder: '',
+  inputValue: '',
 };
 
 export default TextInput;
