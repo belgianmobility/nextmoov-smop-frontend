5f3fd732c0b357ea1e0b80cbb5273e5f6e41653d
Mathieu <mathieu.f@nextmoov.be>
2019-12-08 16:01:32 +0100

    feat(Map): add support for fitbounds and polyline display

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -4,61 +4,81 @@ import { ZoomControl } from 'react-mapbox-gl';
 import MapItem from './MapItem';
 import mapConfig from '../../mapConfig';
 import { getBounds, findBounds } from '../../utils/mapBounds';
+import Polyline from './subcomponents/Polyline';
 
 type Props = {
   lang?: string,
-  center?: Array<number>,
+  position?: Array<number>,
   polylines?: Array<Array<number>>,
+  colors?: Array<string>,
+  modes?: Array<string>,
+  intermediatesStops?: Array<Array<number>>,
 }
 
 const DEFAULT_COORDS = [4.3571273, 50.8452665];
 const DEFAULT_DELTA = 0.001;
 
-
 function Map(props: Props) {
   const {
     lang,
-    center,
+    position,
     polylines,
+    colors,
+    modes,
+    intermediatesStops,
   } = props;
 
-  const hasPolyLines = polylines !== undefined && polylines.length > 0;
+  const [hasPolyLines, setHasPolylines] = useState();
+  const [fitbounds, setFitbounds] = useState(getBounds(DEFAULT_COORDS, DEFAULT_DELTA));
 
-  const [fitbounds, setFitbounds] = useState(
-    hasPolyLines ? findBounds(polylines) : getBounds(DEFAULT_COORDS, DEFAULT_DELTA),
-  );
+  useEffect(() => {
+    if (polylines.length > 0) {
+      setHasPolylines(true);
+    } else {
+      setHasPolylines(false);
+    }
+  }, [polylines, position]);
+
+  useEffect(() => {
+    if (hasPolyLines) {
+      setFitbounds(findBounds(polylines));
+    } else {
+      setFitbounds(getBounds(
+        position !== undefined
+          ? position
+          : DEFAULT_COORDS, DEFAULT_DELTA,
+      ));
+    }
+  }, [hasPolyLines, polylines, position]);
 
   return (
     <MapItem
       fitBounds={fitbounds}
-      fitBoundsOptions={{
-        padding: {
-          top: 40,
-          bottom: 40,
-          left: 40,
-          right: 40,
-        },
-      }}
-      containerStyle={{
-        height: '100vh',
-        width: '70vw',
-        position: 'relative',
-      }}
-      animationOptions={{
-        duration: 0,
-        animate: false,
-      }}
+      fitBoundsOptions={mapConfig(lang).fitboundsOptions}
+      containerStyle={mapConfig(lang).containerStyle}
+      animationOptions={mapConfig(lang).animationOptions}
       style={mapConfig(lang).mapStyle}
     >
       <ZoomControl />
+      {polylines.length > 0 && (
+        <Polyline
+          polylines={polylines}
+          colors={colors}
+          modes={modes}
+          intermediatesStops={intermediatesStops}
+        />
+      )}
     </MapItem>
   );
 }
 
 Map.defaultProps = {
   lang: 'en',
-  center: [],
-  polylines: undefined,
+  position: undefined,
+  polylines: [],
+  colors: [],
+  modes: [],
+  intermediatesStops: [],
 };
 
 export default Map;
