44f7e029a24634ec15d8c14d24c68169246ed89e
Mathieu <mathieu.f@nextmoov.be>
2019-12-08 17:00:01 +0100

    feat(App): search itineraries with from and to places

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 // @flow
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Sidebar from './containers/Sidebar';
-
+import searchTrip from './api/searchTrip';
 
 function App() {
+  const [fromPlace, setFromPlace] = useState();
+  const [toPlace, setToPlace] = useState();
+  const [itineraries, setItineraries] = useState();
+
+  function handleResultClick(e) {
+    const { id, geo } = e;
+    if (id === 'from') {
+      setFromPlace(`${geo[0].lat},${geo[0].lon}`);
+    } if (id === 'to') {
+      setToPlace(`${geo[0].lat},${geo[0].lon}`);
+    }
+  }
+
+  useEffect(() => {
+    if (fromPlace !== undefined && toPlace !== undefined) {
+      searchTrip(fromPlace, toPlace).then((res) => setItineraries(res.plan.itineraries));
+    }
+  }, [fromPlace, toPlace]);
+
+  console.log(itineraries);
+
+
   return (
     <div className="App">
       <Sidebar
-        onResultClick={(e) => console.log(e)}
+        onResultClick={handleResultClick}
       />
     </div>
   );
