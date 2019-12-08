f27e5b9de68dbb7a62d4d1e46295336c1635af75
Mathieu <mathieu.f@nextmoov.be>
2019-12-08 16:58:42 +0100

    feat(api): add searchTrip function

---
+++ src/api/searchTrip.js
@@ -0,0 +1,11 @@
+import config from '../config';
+
+const { API } = config;
+const APIRoute = config.APIRoutes.searchTrip;
+
+function searchTrip(fromPlace, toPlace) {
+  return fetch(`${API}${APIRoute}fromPlace=${fromPlace}&toPlace=${toPlace}&numItineraries=1`)
+    .then((res) => res.json()).catch((err) => console.log(err));
+}
+
+export default searchTrip;
