fe6c2774260ba272115d63537dafe2a34a238288
Mathieu <mathieu.f@nextmoov.be>
2019-12-08 23:40:02 +0100

    feat(Sidebar): add itinerary to sidebar

--- src/containers/Sidebar/index.js
+++ src/containers/Sidebar/index.js
@@ -2,11 +2,15 @@
 import React from 'react';
 import s from './style.module.scss';
 import AutoComplete from '../AutoComplete';
+import Itinerary from '../../components/Itinerary';
 
 type Props = {
   onResultClick?: Function,
   position?: Array<number>,
   lang?: string,
+  itineraries?: Array<Object>,
+  from?: string,
+  to?: string,
 }
 
 function Sidebar(props: Props) {
@@ -14,6 +18,9 @@ function Sidebar(props: Props) {
     onResultClick,
     position,
     lang,
+    itineraries,
+    from,
+    to,
   } = props;
 
   return (
@@ -23,6 +30,23 @@ function Sidebar(props: Props) {
         position={position}
         lang={lang}
       />
+      {itineraries.length > 0 && (
+        itineraries.map((itinerary, index) => {
+          return (
+            <Itinerary
+              key={`${itinerary + index}`}
+              startTime={itinerary.startTime}
+              endTime={itinerary.endTime}
+              duration={itinerary.duration}
+              legs={itinerary.legs}
+              waitingTime={itinerary.waitingTime}
+              walkTime={itinerary.walkTime}
+              from={from}
+              to={to}
+            />
+          );
+        })
+      )}
     </div>
   );
 }
@@ -31,6 +55,9 @@ Sidebar.defaultProps = {
   onResultClick: () => {},
   position: undefined,
   lang: 'en',
+  itineraries: [],
+  from: '',
+  to: '',
 };
 
 export default Sidebar;
