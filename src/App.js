dec83056ae85a6a395eb4646c29ecebac4271462
Mathieu <mathieu.f@nextmoov.be>
2019-12-09 01:10:12 +0100

    feat(App): fetch itinerary details, add map alongside sidebar fetch user position

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 // @flow
 import React, { useState, useEffect } from 'react';
+import mapBoxPolyline from '@mapbox/polyline';
 import Sidebar from './containers/Sidebar';
+import Map from './components/Map';
 import searchTrip from './api/searchTrip';
+import getLegInfo from './utils/getLegInfo';
+import getGeoLocation from './api/getGeoLocation';
 
 function App() {
   const [fromPlace, setFromPlace] = useState();
   const [toPlace, setToPlace] = useState();
-  const [itineraries, setItineraries] = useState();
+  const [itineraries, setItineraries] = useState([]);
+  const [position, setPosition] = useState();
+  const [itineraryDetails, setItineraryDetails] = useState({
+    polyline: [],
+    colors: [],
+    modes: [],
+    intermediateStops: [],
+  });
 
   function handleResultClick(e) {
     const { id, geo, name } = e;
@@ -29,13 +40,57 @@ function App() {
     }
   }, [fromPlace, toPlace]);
 
+  useEffect(() => {
+    getGeoLocation()
+      .then((res) => setPosition([res.coords.longitude, res.coords.latitude]))
+      .catch(() => setPosition());
+  }, []);
+
+  useEffect(() => {
+    let itineraryColors;
+    let itineraryModes;
+    const itineraryPolyLine = [];
+    const itineraryIntermediateStops = [];
+    if (itineraries.length > 0) {
+      const legsInfos = getLegInfo(itineraries[0].legs);
+      itineraryColors = legsInfos.map((info, index) => legsInfos[index].legColor);
+      itineraryModes = legsInfos.map((info, index) => legsInfos[index].legMode);
+      itineraries[0].legs.map((leg) => {
+        itineraryIntermediateStops.push(leg.intermediateStops);
+        return itineraryPolyLine.push(mapBoxPolyline.decode(leg.legGeometry.points));
+      });
+      setItineraryDetails({
+        modes: itineraryModes,
+        colors: itineraryColors,
+        polyline: itineraryPolyLine,
+        intermediateStops: itineraryIntermediateStops,
+      });
+    }
+  }, [itineraries]);
+
   return (
-    <div className="App">
+    <div
+      className="App"
+      style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        height: '100vh',
+        overflow: 'hidden',
+      }}
+    >
       <Sidebar
         onResultClick={handleResultClick}
         itineraries={itineraries}
         from={fromPlace !== undefined ? fromPlace.name : ''}
         to={toPlace !== undefined ? toPlace.name : ''}
+        position={position}
+      />
+      <Map
+        polylines={itineraryDetails.polyline}
+        colors={itineraryDetails.colors}
+        modes={itineraryDetails.modes}
+        intermediateStops={itineraryDetails.intermediateStops}
+        position={position}
       />
     </div>
   );
